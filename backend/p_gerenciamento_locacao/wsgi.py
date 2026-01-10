57c611f648b418e9f1dd111283fd1303b7c7f1d0
GHRodriguess <gabri.henri20088@gmail.com>
2026-01-10 12:22:14 -0300

    iniciando projeto django

---
+++ backend/p_gerenciamento_locacao/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for p_gerenciamento_locacao project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'p_gerenciamento_locacao.settings')
+
+application = get_wsgi_application()
