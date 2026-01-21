bfad0259d1ad2075fe1b3e2a9d209ea3806f6e34
GHRodriguess <gabri.henri20088@gmail.com>
2026-01-21 13:06:38 -0300

    rodando collectstatic

--- backend/p_gerenciamento_locacao/settings.py
+++ backend/p_gerenciamento_locacao/settings.py
@@ -154,7 +154,8 @@ AUTH_USER_MODEL = 'a_users.User'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 SPECTACULAR_SETTINGS = {
     'TITLE': 'Gerenciamento de Locação API',
